87ab45ed5dcda2cd3d7503b63889ad64eadda842
Rachamv <rkutalian@gmail.com>
2023-11-19 06:45:33 +0100

    done

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from collections import Counter
+
 def create_meal_frequencies(preferences):
-    pass
+    meal_frequencies = Counter()
 
-def create_user_scores(preferences, meal_score):
-    pass
+    for meals in preferences.values():
+        meal_frequencies.update(meals)
 
-def find_users_with_lowest_score(user_score):
-    pass
+    return meal_frequencies
+
+def create_user_scores(preferences, meal_frequencies):
+    user_scores = {person: sum(meal_frequencies[meal] for meal in meals) for person, meals in preferences.items()}
+
+    return user_scores
+
+def find_users_with_lowest_score(user_scores):
+    min_score = min(user_scores.values())
+    lowest_score_users = [user for user, score in user_scores.items() if score == min_score]
+
+    return lowest_score_users
 
 if __name__ == '__main__':
     preferences = {
@@ -13,7 +25,6 @@ if __name__ == '__main__':
         'b': {'pizza', 'shrimp', 'tea'},
         'c': {'pizza', 'nutella', 'dumplings'},
     }
-
 
     meal_score = create_meal_frequencies(preferences)
     print(f'{meal_score}')
